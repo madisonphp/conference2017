31650456c5abfd4f0f90c4649919d279fa4849ea
Andrew Shell <andrew@andrewshell.org>
2017-03-12 17:22:18 -0700

    Trying to load composer in the browser

--- web/gitpull.php
+++ web/gitpull.php
@@ -1,4 +1,8 @@
 <?php
+use Composer\Factory;
+use Composer\XdebugHandler;
+use Composer\Console\Application;
+
 date_default_timezone_set('America/Chicago');
 
 header("Content-type: text/plain"); // be explicit to avoid accidental XSS
@@ -6,4 +10,14 @@ header("Content-type: text/plain"); // be explicit to avoid accidental XSS
 $gitpath = '/usr/local/bin/git';
 chdir(__DIR__ . '/../'); // rarely actually an acceptable thing to do
 system("/usr/bin/env -i {$gitpath} pull 2>&1"); // main repo (current branch)
-system("/usr/bin/env -i COMPOSER_HOME=./.composer /usr/local/bin/composer install 2>&1"); // composer
+
+error_reporting(-1);
+
+// Create output for XdebugHandler and Application
+$output = Factory::createOutput();
+
+putenv('COMPOSER_BINARY='.realpath(__FILE__));
+
+// run the command application
+$application = new Application();
+$application->run(null, $output);
